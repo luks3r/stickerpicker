36d2712e6cc2d0feba26210fe5bfa2d920882de6
Sergei Lukaushkin <sergeilukaushkin@gmail.com>
2024-12-23 23:35:43 +0200

    feat: add more emojis

--- build/lib/sticker/version.py
+++
@@ -1,6 +0,0 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = '89d3aece'
-version = '0.1.0+dev.89d3aece'
-linkified_version = '0.1.0+dev.[89d3aece](https://github.com/maunium/stickerpicker/commit/89d3aece041c85ebe5a1ad4e620388af5227cbb0)'
